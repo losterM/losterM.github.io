41a74321d9be425bc8da504a91228831e1baa039
MaXuSun <2372883821@qq.com>
2020-03-18 18:21:34 +0800

    初始化框架

---
+++ precache-manifest.87f07d0445ef625b1401ad2219a195dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e6d625d807ed499e84c31e9be5f726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b7aad09ffd8dd93a19",
+    "url": "/static/css/2.9c750508.chunk.css"
+  },
+  {
+    "revision": "9c7e1843177830b7645c",
+    "url": "/static/css/main.f3b51cfa.chunk.css"
+  },
+  {
+    "revision": "95b7aad09ffd8dd93a19",
+    "url": "/static/js/2.09f13ccf.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/static/js/2.09f13ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7e1843177830b7645c",
+    "url": "/static/js/main.9f33efd5.chunk.js"
+  },
+  {
+    "revision": "0aca63a31565be9eec69",
+    "url": "/static/js/runtime-main.c8ae74a9.js"
+  },
+  {
+    "revision": "1cb9e662760ed0abd4e80df4d914e2b3",
+    "url": "/static/media/gbqq.1cb9e662.mp3"
+  }
+]);
